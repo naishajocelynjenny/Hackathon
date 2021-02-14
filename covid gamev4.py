71214a4eceaebfb2f1bc39608ae902c2f163679a
NaishaAgarwal <54164228+NaishaAgarwal@users.noreply.github.com>
2021-02-13 16:53:11 -0800

    Update covid gamev4.py

--- covid gamev4.py
+++ covid gamev4.py
@@ -1,14 +1,16 @@
 # 1 - Import library
 import pygame
 import time
+import random
 from pygame.locals import *
 
 # 2 - Initialize the game
 pygame.init()
 display_width = 575
 display_height = 575
+coronavirus_height = 60
 gameDisplay=pygame.display.set_mode((display_width, display_height))
-pygame.display.set_caption('PLACEHOLDER')
+pygame.display.set_caption('ITS RAINING CORONA')
 black = (0,0,0)
 white = (255,255,255)
 red = (255,0,0)
@@ -17,8 +19,8 @@ player_height = 73
 clock = pygame.time.Clock()
 
 # 3 - Load images
-playerImg = pygame.image.load("/Users/Jocelyn/Downloads/main character (1).png")
-coronaImg = pygame.image.load("/Users/Jocelyn/Downloads/Coronavirus .png")
+playerImg = pygame.image.load("/Users/bharatiagarwal/Downloads/main character (1).png")
+coronaImg = pygame.image.load("/Users/bharatiagarwal/Downloads/Coronavirus (1).png")
 
 # 4 - keep looping through
 
@@ -49,7 +51,7 @@ def crash():
 
 def game_loop(): 
     x =  (display_width * 0.45)
-    y = (display_height * 0.8)
+    y = (display_height * 0.45)
     x_change = 0
     y_change = 0
     player_speed = 0
@@ -67,30 +69,44 @@ def game_loop():
             
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
-                    x_change = -5
+                    x_change = -6
                 elif event.key == pygame.K_RIGHT:
-                    x_change = 5
+                    x_change = 6
                 elif event.key == pygame.K_UP:
-                    y_change = -5
+                    y_change = -6
                 elif event.key == pygame.K_DOWN:
-                    y_change = 5
+                    y_change = 6
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_UP or event.key == pygame.K_DOWN:
                     x_change = 0
                     y_change = 0
+                    
 
         x += x_change
         y += y_change
 
         gameDisplay.fill(white)
+        coronavirus(coronavirus_startx, coronavirus_starty)
+        coronavirus_starty += coronavirus_speed
         player(x,y)
+       
 
         if x > display_width - player_width or x < 0:
             x_change = 0
 
         if y > display_height - player_height or y < 0:
             y_change = 0
-    
+
+        if coronavirus_starty  > display_height:
+                coronavirus_starty = 0 - coronavirus_height
+                coronavirus_startx = random.randrange(0, display_width)
+        if y <= coronavirus_starty + coronavirus_height - 50 and y>= coronavirus_starty - coronavirus_height - 60 or y - player_height <= coronavirus_starty + coronavirus_height - 60 and y - player_height >= coronavirus_starty -coronavirus_height - 60:
+            #('y crossover')
+
+            if x >= coronavirus_startx and x<= coronavirus_startx + coronavirus_height - 40 or x + player_width >= coronavirus_startx  and x + player_width <= coronavirus_startx+ coronavirus_height - 40:
+               # print('x crossover')
+                crash()
+                
         pygame.display.update()
         clock.tick(60)
 
